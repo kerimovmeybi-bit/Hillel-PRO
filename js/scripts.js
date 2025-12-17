a69da0932f50373d3baa3494459d778b304573d8
Meybi Kerimli <kerimovmeybi@gmail.com>
2025-12-17 09:44:32 +0200

    Modified the file scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
+//Home-Work 3.1
+// Number
+let number = 10;
+console.log(number, typeof number);
+ 
+//String
+let text = "Hello";
+console.log(text, typeof text);
+
+//Boolean
+let isText = true;
+console.log(isText, typeof isText);
+
+
+
+//Home-Work 3.2
 const str1 = prompt ("Перший рядок");
 const str2 = prompt ("Другий рядок");
 const str3 = prompt ("Третій рядок");
 
 console.log(`${str3} ${str1} ${str2}`);
 
-const number = prompt("10369");
-console.log(number.split('').join(' '));
+
+
+//Home-Work 3.3
+const n = prompt("10369");
+console.log(n.split('').join(' '));
