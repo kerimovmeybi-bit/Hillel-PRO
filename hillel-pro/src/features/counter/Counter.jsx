9fde45b843b42602a456bf270d7ab3f2eb07e3d7
Meybi Kerimli <kerimovmeybi@gmail.com>
2026-03-26 08:05:23 +0200

    add Home-Work29

---
+++ hillel-pro/src/features/counter/Counter.jsx
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './counterSlice';
+
+export default function Counter() {
+  const value = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Value: {value}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+}
--- hillel-pro/src/main.jsx
+++ hillel-pro/src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './app/store.js';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </BrowserRouter>
+  </Provider>
 );
